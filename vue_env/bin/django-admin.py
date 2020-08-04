18a83f54616ba0f134b2022a4febe42c0307150f
Jasmine Smith <jasminesmit1@unomaha.edu>
2020-08-04 12:27:14 -0500

    initial commit

---
+++ vue_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jasminesmith/Desktop/ISQA8380/Assign3/efs_rest/vue_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
